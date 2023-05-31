b826919926b2808347a6b4e4c150616d8a95f1b3
Álvaro Manjón <alvaro.manjon@me.com>
2023-05-31 21:42:00 +0200

    feat: Add new NutriMenuEnterprise Spring-Boot proyect (#3)

---
+++ webapps/NutriMenuEnterprise/src/main/java/com/alvaromanjon/nutrimenuenterprise/NutriMenuEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.alvaromanjon.nutrimenuenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriMenuEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriMenuEnterpriseApplication.class, args);
+	}
+
+}
