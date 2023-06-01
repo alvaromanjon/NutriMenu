a679bad123ebd344103c552da74595946fb0ac47
Álvaro Manjón Vara <alvaro.manjon@me.com>
2023-06-01 11:30:32 +0200

    feat: Add new client webapp project (#3)

---
+++ webapps/nutrimenuclient/src/main/java/ooo/alvar/nutrimenuclient/NutrimenuclientApplication.java
@@ -0,0 +1,13 @@
+package ooo.alvar.nutrimenuclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimenuclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrimenuclientApplication.class, args);
+	}
+
+}
---
+++ webapps/nutrimenuclient/src/test/java/ooo/alvar/nutrimenuclient/NutrimenuclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ooo.alvar.nutrimenuclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrimenuclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
