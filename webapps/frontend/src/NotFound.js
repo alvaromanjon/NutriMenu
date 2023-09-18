00d4849d92b39b1d5474538d68719485bb82ee18
Álvaro Manjón <alvaro.manjon@me.com>
2023-09-18 19:56:17 +0200

    feat: Update design on Not Found page (#22)

--- webapps/frontend/src/NotFound.js
+++ webapps/frontend/src/NotFound.js
@@ -1,12 +1,22 @@
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h2>Creo que te has equivocado 🤕</h2>
-      <p>La página a la que acabas de intentar acceder no existe :(</p>
-      <Link to="/">Vuelve a la página de inicio</Link>
-    </div>
+    <Container fluid className="d-flex justify-content-center align-items-center vh-100">
+      <Card
+        className="d-flex flex-column justify-content-center align-items-center py-4 shadow-lg"
+        style={{ width: "600px", height: "250px" }}
+      >
+        <Card.Body>
+          <h2 className="px-6">Creo que te has equivocado 🤕</h2>
+          <p className="px-6">La página a la que acabas de intentar acceder no existe :(</p>
+          <Button className="mt-2 mb-3" as={Link} to="/">
+            Vuelve a la página de inicio
+          </Button>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
