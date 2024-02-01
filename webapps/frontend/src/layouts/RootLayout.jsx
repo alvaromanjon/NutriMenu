ce087fe89d5bdb3c0ebabd2d4f41ce6040074c4f
Álvaro Manjón <alvaro.manjon@me.com>
2024-02-01 19:40:27 +0100

    fix: Add collapse to navbar (#22)

--- webapps/frontend/src/layouts/RootLayout.jsx
+++ webapps/frontend/src/layouts/RootLayout.jsx
@@ -1,13 +1,12 @@
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Button from "react-bootstrap/Button";
-import { useContext } from "react";
+import { Nav, Navbar, Button } from "react-bootstrap";
+import { useContext, useState } from "react";
 import { Outlet, NavLink, useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 const RootLayout = () => {
-  const { usuario, setUsuario } = useContext(UserContext);
+  const { usuario } = useContext(UserContext);
   const navigate = useNavigate();
+  const [expanded, setExpanded] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem("usuario-sesion");
@@ -19,16 +18,16 @@ const RootLayout = () => {
     return (
       <>
         <Nav className="mr-auto">
-          <Nav.Link className="ms-3" as={NavLink} to={"/empresas/table"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/empresas/table"} onClick={() => setExpanded(false)}>
             Gestión de empresas
           </Nav.Link>
-          <Nav.Link className="ms-3" as={NavLink} to={"/locales/table"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/locales/table"} onClick={() => setExpanded(false)}>
             Gestión de locales
           </Nav.Link>
-          <Nav.Link className="ms-3" as={NavLink} to={"/usuarios/table"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/usuarios/table"} onClick={() => setExpanded(false)}>
             Gestión de usuarios
           </Nav.Link>
-          <Nav.Link className="ms-3" as={NavLink} to={"/alimentos/table"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/alimentos/table"} onClick={() => setExpanded(false)}>
             Gestión de alimentos
           </Nav.Link>
         </Nav>
@@ -45,10 +44,10 @@ const RootLayout = () => {
     return (
       <>
         <Nav className="mr-auto">
-          <Nav.Link className="ms-3" as={NavLink} to={"/menus"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/menus"} onClick={() => setExpanded(false)}>
             Gestión de menús
           </Nav.Link>
-          <Nav.Link className="ms-3" as={NavLink} to={"/platos"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/platos"} onClick={() => setExpanded(false)}>
             Gestión de platos
           </Nav.Link>
         </Nav>
@@ -65,7 +64,7 @@ const RootLayout = () => {
     return (
       <>
         <Nav className="mr-auto">
-          <Nav.Link className="ms-3" as={NavLink} to={"/menus"}>
+          <Nav.Link className="ms-3" as={NavLink} to={"/menus"} onClick={() => setExpanded(false)}>
             Gestión de menús
           </Nav.Link>
         </Nav>
@@ -82,7 +81,13 @@ const RootLayout = () => {
     return (
       <>
         <Nav className="ms-auto">
-          <Button className="mx-3 my-2" variant="outline-light" as={NavLink} to="/login">
+          <Button
+            className="mx-3 my-2"
+            variant="outline-light"
+            as={NavLink}
+            to="/login"
+            onClick={() => setExpanded(false)}
+          >
             Iniciar sesión
           </Button>
         </Nav>
@@ -91,13 +96,17 @@ const RootLayout = () => {
   };
 
   return (
-    <div>
+    <>
       <header>
-        <Navbar className="shadow" bg="dark" variant="dark" expand="lg">
+        <Navbar className="shadow" bg="dark" variant="dark" expand="lg" expanded={expanded}>
           <Navbar.Brand className="ms-3" href="/">
             NutriMenu
           </Navbar.Brand>
-          <Navbar.Toggle className="me-3" aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            className="me-3"
+            aria-controls="basic-navbar-nav"
+            onClick={() => setExpanded(expanded ? false : "expanded")}
+          />
           <Navbar.Collapse id="basic-navbar-nav">
             {usuario.rol === "ADMINISTRADOR" && adminNavBar()}
             {usuario.rol === "EDITOR" && editorNavBar()}
@@ -109,7 +118,7 @@ const RootLayout = () => {
       <main>
         <Outlet />
       </main>
-    </div>
+    </>
   );
 };
 
