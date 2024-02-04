7c502455e63cd093fdb7fdc7e02d5c690f99bd09
Álvaro Manjón <alvaro.manjon@me.com>
2024-02-04 20:00:22 +0100

    feat: Improve layout when creating nutrient (#22)

--- webapps/frontend/src/pages/alimentos/new/NewAlimento.jsx
+++ webapps/frontend/src/pages/alimentos/new/NewAlimento.jsx
@@ -1,4 +1,4 @@
-import { Container, Button, Card, Row, Col } from "react-bootstrap";
+import { Container, Card, Row, Col, Nav } from "react-bootstrap";
 import { Outlet, NavLink } from "react-router-dom";
 
 const NewAlimento = () => {
@@ -6,19 +6,24 @@ const NewAlimento = () => {
     <Container className="mt-5">
       <h3 className="text-center">Creación de un nuevo alimento</h3>
       <h5 className="text-center mt-3">¿Cómo quieres crear el alimento?</h5>
-      <Row className="justify-content-center mb-4">
-        <Col xs="auto">
-          <Button variant="primary" as={NavLink} to="search" className="me-2 mt-2">
-            Buscar en Nutritionix
-          </Button>
-          <Button variant="primary" className="ms-2 mt-2" as={NavLink} to="create">
-            Crear a mano
-          </Button>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
+
+      <Row className="justify-content-center mt-4">
         <Col md={10}>
           <Card>
+            <Card.Header>
+              <Nav variant="tabs">
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="search">
+                    Buscar en Nutritionix
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="create">
+                    Crear a mano
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Card.Header>
             <Card.Body className="p-4">
               <Outlet />
             </Card.Body>
