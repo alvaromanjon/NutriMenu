2cbd0cdfceba744b8916a9bec599fca1ceeaee01
Álvaro Manjón <alvaro.manjon@me.com>
2024-02-04 21:43:49 +0100

    chore: Modify Loading component (#22)

--- webapps/frontend/src/utils/Loading.jsx
+++ webapps/frontend/src/utils/Loading.jsx
@@ -1,11 +1,13 @@
-import { Spinner, Container } from "react-bootstrap";
+import { Spinner, Container, Row } from "react-bootstrap";
 
 const Loading = () => {
   return (
-    <Container className="m-3">
-      <Spinner animation="grow" role="status">
-        <span className="visually-hidden">Cargando...</span>
-      </Spinner>
+    <Container>
+      <Row className="justify-content-center mt-4">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Cargando...</span>
+        </Spinner>
+      </Row>
     </Container>
   );
 };
