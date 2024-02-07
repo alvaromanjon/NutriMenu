2e21f9fa02a42b72aa8a27974d66eebc4508a49b
Álvaro Manjón <alvaro.manjon@me.com>
2024-02-07 18:42:52 +0100

    feat: Route data from menu info component to global context (#22)

--- webapps/frontend/src/pages/menus/new/NewMenu.jsx
+++ webapps/frontend/src/pages/menus/new/NewMenu.jsx
@@ -1,13 +1,22 @@
-import { Alert, Button, Card, Col, Container, Nav, Row } from "react-bootstrap";
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { Alert, Button, Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../../contexts/UserContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { WarningActionModal } from "../../../utils/WarningActionModal";
 import NewMenuInformation from "./NewMenuInformation";
 import NewMenuListLocales from "./NewMenuListLocales";
 import NewMenuAddFromList from "./NewMenuAddFromList";
+import { usePlatosLocalesMenu } from "../../../store/platosLocalesMenu";
 
 const NewMenu = () => {
+  const resetState = usePlatosLocalesMenu((state) => state.reset);
+  const [nombre, descripcion, fechaPublicacion, locales, platos] = usePlatosLocalesMenu((state) => [
+    state.nombre,
+    state.descripcion,
+    state.fechaPublicacion,
+    state.locales,
+    state.platos,
+  ]);
   const navigate = useNavigate();
   const { usuario } = useContext(UserContext);
   const [showModal, setShowModal] = useState(false);
@@ -15,6 +24,14 @@ const NewMenu = () => {
   const handleCloseModal = () => setShowModal(false);
   const [errorFlag, setErrorFlag] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    console.log("Nombre: ", nombre);
+    console.log("Descripción: ", descripcion);
+    console.log("Fecha de publicación: ", fechaPublicacion);
+    console.log("Locales: ", locales);
+    console.log("Platos: ", platos);
+  }, [nombre, descripcion, fechaPublicacion, locales, platos]);
 
   return (
     <>
@@ -51,7 +68,7 @@ const NewMenu = () => {
             firstButton="No"
             secondButton="Sí, cancelar"
             action={() => {
-              //resetState();
+              resetState();
               navigate("/menus");
             }}
           />
--- webapps/frontend/src/pages/menus/new/NewMenuInformation.jsx
+++ webapps/frontend/src/pages/menus/new/NewMenuInformation.jsx
@@ -1,6 +1,18 @@
 import { Card, Form } from "react-bootstrap";
+import { usePlatosLocalesMenu } from "../../../store/platosLocalesMenu";
 
 const NewMenuInformation = () => {
+  const [nombre, descripcion, fechaPublicacion, setNombre, setDescripcion, setFechaPublicacion] = usePlatosLocalesMenu(
+    (state) => [
+      state.nombre,
+      state.descripcion,
+      state.fechaPublicacion,
+      state.setNombre,
+      state.setDescripcion,
+      state.setFechaPublicacion,
+    ],
+  );
+
   return (
     <Card className="mb-4">
       <Card.Header>
@@ -10,15 +22,32 @@ const NewMenuInformation = () => {
         <Form className="justify-content-md-center">
           <Form.Group className="mb-3">
             <Form.Label>Nombre del menú</Form.Label>
-            <Form.Control name="nombre" type="text" placeholder="Menú del viernes" />
+            <Form.Control
+              name="nombre"
+              type="text"
+              placeholder="Menú del viernes"
+              value={nombre}
+              onChange={(e) => setNombre(e.currentTarget.value)}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Descripción del menú</Form.Label>
-            <Form.Control name="descripcion" type="text" placeholder="Este es el menú del día de los viernes" />
+            <Form.Control
+              name="descripcion"
+              type="text"
+              placeholder="Este es el menú del día de los viernes"
+              value={descripcion}
+              onChange={(e) => setDescripcion(e.currentTarget.value)}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Fecha de publicación del menú</Form.Label>
-            <Form.Control name="fecha" type="date" />
+            <Form.Control
+              name="fechaPublicacion"
+              type="date"
+              value={fechaPublicacion}
+              onChange={(e) => setFechaPublicacion(e.currentTarget.value)}
+            />
           </Form.Group>
         </Form>
       </Card.Body>
