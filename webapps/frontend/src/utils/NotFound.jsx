6387560fc1198b9f9e4299e8981068487e3f5f0c
Álvaro Manjón <alvaro.manjon@me.com>
2024-02-09 17:54:52 +0100

    fix: Remove unnecessary scrolling on Not Found page (#22)

--- webapps/frontend/src/utils/NotFound.jsx
+++ webapps/frontend/src/utils/NotFound.jsx
@@ -1,25 +1,24 @@
-import { Container, Card, Button } from "react-bootstrap";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../pages/login/styles.css";
 
 const NotFound = () => {
   return (
-    <div className="gradient">
-      <Container fluid className="d-flex justify-content-center align-items-center vh-100">
-        <Card
-          className="d-flex flex-column justify-content-center align-items-center py-4 shadow-lg"
-          style={{ width: "600px", height: "250px" }}
-        >
-          <Card.Body>
-            <h2 className="px-6">Creo que te has equivocado 🤕</h2>
-            <p className="px-6 mt-3">La página a la que acabas de intentar acceder no existe :(</p>
-            <Button className="mt-2 mb-3" as={Link} to="/">
-              Vuelve a la página de inicio
-            </Button>
-          </Card.Body>
-        </Card>
-      </Container>
-    </div>
+    <Container fluid className="position-relative">
+      <Row className="justify-content-center align-items-center" style={{ height: "80vh" }}>
+        <Col>
+          <Card className="text-center shadow-lg pt-5 mx-auto" style={{ maxWidth: "700px", height: "275px" }}>
+            <Card.Body>
+              <h2 className="px-6">Creo que te has equivocado 🤕</h2>
+              <p className="px-6 mt-3">La página a la que acabas de intentar acceder no existe :(</p>
+              <Button variant="dark" className="mt-2 mb-3" as={Link} to="/">
+                Vuelve a la página de inicio
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
