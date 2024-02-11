2bec0eaebbc6b976f3049d25248d10aa908790aa
Álvaro Manjón <alvaro.manjon@me.com>
2024-02-11 21:38:42 +0100

    feat: Add Menu selection graph (#22)

--- webapps/frontend/src/pages/menus/view/MenuDetailsGraph.jsx
+++ webapps/frontend/src/pages/menus/view/MenuDetailsGraph.jsx
@@ -3,16 +3,34 @@ import { AgChartsReact } from "ag-charts-react";
 import { calculateMenuNutrients } from "../../../utils/calculateMenuNutrients";
 
 const MenuDetailsGraph = ({ entrante, primerPlato, segundoPlato, postre }) => {
-  const [data, setData] = useState({});
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     const menuSumado = calculateMenuNutrients(entrante, primerPlato, segundoPlato, postre);
-
-    console.log(menuSumado);
-    setData(menuSumado);
+    const newData = [
+      { tipo: "Grasas", valor: menuSumado.grasas },
+      { tipo: "Grasas saturadas", valor: menuSumado.grasasSaturadas },
+      { tipo: "Hidratos de carbono", valor: menuSumado.hidratosCarbono },
+      { tipo: "Azúcares", valor: menuSumado.azucares },
+      { tipo: "Fibra", valor: menuSumado.fibra },
+      { tipo: "Proteínas", valor: menuSumado.proteinas },
+      { tipo: "Sal", valor: menuSumado.sal },
+    ];
+    setChartData(newData);
+    //console.log(data);
   }, [entrante, primerPlato, segundoPlato, postre]);
 
-  return <></>;
+  const options = {
+    autoSize: true,
+    data: chartData,
+    series: [{ type: "pie", angleKey: "valor", legendItemKey: "tipo" }],
+  };
+
+  return (
+    <>
+      <AgChartsReact options={options} />
+    </>
+  );
 };
 
 export default MenuDetailsGraph;
